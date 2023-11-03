f0c2aa22fc316735c31dcb774f1297b3f1d31a00
miltonh-github <119451755+miltonh-github@users.noreply.github.com>
2023-11-03 09:03:55 -0600

    Layout Components completed

--- App.js
+++ App.js
@@ -1,20 +1,85 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+/**
+ * My To Do List App
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+} from 'react-native';
+import ToDoList from './ToDoList';
+import ToDoForm from './ToDoForm';
+
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <ToDoList />
+      <ToDoForm />
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   task: {
+//     padding: 10,
+//     borderBottomWidth: 1,
+//     borderColor: '#ccc',
+//   },
+//   completed: {
+//     backgroundColor: '#e0e0e0',
+//   },
+//   taskText: {
+//     fontSize: 16,
+//   },
+//   form: {
+//     flexDirection: 'row',
+//     justifyContent: 'space-between',
+//     alignItems: 'center',
+//     marginHorizontal: 20,
+//     marginTop: 20,
+//   },
+//   input: {
+//     flex: 1,
+//     borderWidth: 1,
+//     borderColor: '#ccc',
+//     paddingHorizontal: 10,
+//     paddingVertical: 5,
+//     marginRight: 10,
+//   },
+// });
+
+export default App;
+
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+
+// import {
+//   SafeAreaView,
+//   Pressable,
+//   ScrollView,
+//   TextInput,
+//   Button
+// } from 'react-native';
+
+
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
